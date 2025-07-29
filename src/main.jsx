1ee4955314a68d6d69ca0130621f22ceabfdf636
E-ctrl-coder <elbagire@gmail.com>
2025-07-29 22:09:36 +0000

    feat(ui): import index.css in entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// import Tailwind + your custom styles
+import './index.css';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
