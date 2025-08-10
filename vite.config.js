e72ea99662704d2e3f211ba9cd9c580fda6d2886
E-ctrl-coder <elbagire@gmail.com>
2025-08-10 19:28:21 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: '.',
-  publicDir: 'public',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  server: {
-    port: 3000
-  },
-  build: {
-    outDir: 'dist'
-  }
+  base: './', // ensures relative paths for Netlify
 });
