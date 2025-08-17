7c072d448885d04053c8f5d3c38d2c524912038a
E-ctrl-coder <elbagire@gmail.com>
2025-08-17 17:37:29 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: resolve(__dirname, 'عن التطبيق.html'), dest: 'dist' },
+        { src: resolve(__dirname, 'كيفية الاستخدام.html'), dest: 'dist' },
+        { src: resolve(__dirname, 'التسجيل.html'), dest: 'dist' },
+        { src: resolve(__dirname, 'اتصل بنا.html'), dest: 'dist' },
+        { src: resolve(__dirname, 'الإشعارات.html'), dest: 'dist' },
+        { src: resolve(__dirname, 'الروابط.html'), dest: 'dist' },
+        { src: resolve(__dirname, 'إعلانات تجارية.html'), dest: 'dist' }
+      ]
+    })
+  ],
   base: './',
   build: {
     outDir: 'dist',
