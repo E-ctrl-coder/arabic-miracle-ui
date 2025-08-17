37e17cf8ffcaf81d26979d99d1774385eea0b68b
E-ctrl-coder <elbagire@gmail.com>
2025-08-17 18:53:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    copy({
-      targets: [
-        { src: resolve(__dirname, 'عن التطبيق.html'), dest: 'dist' },
-        { src: resolve(__dirname, 'كيفية الاستخدام.html'), dest: 'dist' },
-        { src: resolve(__dirname, 'التسجيل.html'), dest: 'dist' },
-        { src: resolve(__dirname, 'اتصل بنا.html'), dest: 'dist' },
-        { src: resolve(__dirname, 'الإشعارات.html'), dest: 'dist' },
-        { src: resolve(__dirname, 'الروابط.html'), dest: 'dist' },
-        { src: resolve(__dirname, 'إعلانات تجارية.html'), dest: 'dist' }
-      ]
-    })
-  ],
-  base: './',
+  plugins: [react()],
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  },
-  server: {
-    cors: true
+    outDir: 'dist'
   }
 });
